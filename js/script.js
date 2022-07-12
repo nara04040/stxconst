21faefc86999843df659308de9324f4fc0500f1d
Dev.Jin <nara04040@naver.com>
2022-07-12 16:34:40 +0900

    fix header javascript

--- js/script.js
+++ js/script.js
@@ -1,14 +1,25 @@
 $(document).ready(function () {
+    // html 태그의 lang을 알아보자. (attr수업)
+    // let lang = $('nav').attr('gogo', '55');
+    // console.log(lang)
+
     // .header를 저장한다.
     let header = $('.header');
     let gnb = $('.gnb');
+    // 펼칠때 높이
+    let gnbMaxHeight = gnb.outerHeight();
+    // 닫혀있을 때 높이
+    let gnbMinHeight = header.outerHeight();
+    // 기본 header에 높이값 적용
+    header.css('height',gnbMinHeight)
+
 
     gnb.mouseenter(function () {
-        header.addClass('header-open');
+        header.css('height', gnbMaxHeight);
     });
 
     gnb.mouseleave(function () {
-        header.removeClass('header-open');
+        header.css('height', gnbMinHeight);
     });
 
     // 주메뉴 포커스 기능
@@ -26,8 +37,12 @@ $(document).ready(function () {
 
     //컨텐츠 이동버튼
     let visualBt = $('.visual-bt');
+    // .part 영역이 html 상단으로부터 어느만큼 떨어져있는가
+    let partY = $('.part').offset().top;
     visualBt.click(function(){
-
+        $('html').animate({
+            scrollTop: partY
+        });
     });
 
     //part 배경배치
@@ -37,7 +52,8 @@ $(document).ready(function () {
         let tgx = partListW * index;
         tgx = -1 * tgx
         $(this).css('background-position-x', tgx);
-    })
+    });
+
 });
 
 window.onload = function () {}
